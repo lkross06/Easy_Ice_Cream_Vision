cfc768f32640f7d4de0dc471beb6a63a0ff97392
Lucas Kalani Ross <lkross@Lucas-MacBook-Pro.local>
2025-03-02 14:13:55 -0800

    got rid of class architecture for app.py due to recursion issues

--- app.py
+++ app.py
@@ -5,85 +5,87 @@ import time
 from gaze_tracking import GazeTracking
 from mouse_controller import MouseController
 
+######### INITIALIZATION #############
 
-class Application:
-    def __init__(self):
-        #create a mouse controller for emulation
-        self.mc = MouseController()
+#delay the gaze tracking by software by x milliseconds so
+#the mouse controller isnt lagging
+GAZE_DELAY = 50
+#however we want the mouse movement to be smooth, so it can update instantaneously
+MOUSE_DELAY = 10;
+MS_ELAPSED = 0;
 
-        #create a keyboard listener for listening
-        # kl = Listener(on_press=self.handle_key_press)
-        # kl.start()
+keep_updating = True
 
-        #set up gaze tracker
-        self.gaze_tracker = GazeTracking()
-        self.webcam = cv2.VideoCapture(0) #will feed in frames from the laptop's webcam
-        
-        #delay the gaze tracking by software by x milliseconds so
-        #the mouse controller isnt lagging
-        self.gaze_delay = 50
-        #however we want the mouse movement to be smooth, so it can update instantaneously
-        self.mouse_delay = 10;
-        self.ms_elapsed = 0;
-        self.keep_updating = True
+def handle_key_press(event):
+    try:
+        #for keyboard keys
+        if event.char == 'q':
+            print("Q PRESSED")
+            global keep_updating
+            keep_updating = False
+    except AttributeError:
+        #for other keys (cmd, shift, tab, etc.)
+        return
 
-        # #create a separate thread for processing each frame of
-        # #webcam capture and figuring out pupil position/gaze direction
-        # self.gaze_thread = threading.Thread(target=self.update)
-        # self.gaze_thread.start()
 
-        # self.root.mainloop() #tkinter MUST be on main thread
-        self.update()
+#create a mouse controller for emulation
+mc = MouseController()
 
-    def update(self):
-        if self.ms_elapsed % self.gaze_delay == 0:
-            _, frame = self.webcam.read()
+#create a keyboard listener for listening
+kl = Listener(on_press=handle_key_press)
+kl.start()
 
-            #analyze and update gaze tracker
-            self.gaze_tracker.refresh(frame)
+#set up gaze tracker
+gaze_tracker = GazeTracking()
+webcam = cv2.VideoCapture(0) #will feed in frames from the laptop's webcam
 
-            #add markers for pupil location to frame image data
-            frame = self.gaze_tracker.annotated_frame()
 
-            text = "N/A"
-            if self.gaze_tracker.is_blinking():
-                text = "Blinking"
-            elif self.gaze_tracker.is_right():
-                text = "Looking right"
-            elif self.gaze_tracker.is_left():
-                text = "Looking left"
-            elif self.gaze_tracker.is_center():
-                text = "Looking center"
+######### UPDATE LOOP #############
+while True:
+    if MS_ELAPSED % GAZE_DELAY == 0:
+        _, frame = webcam.read()
 
-            cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
+        #analyze and update gaze tracker
+        gaze_tracker.refresh(frame)
 
-            left_pupil = self.gaze_tracker.pupil_left_coords()
-            right_pupil = self.gaze_tracker.pupil_right_coords()
-            cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
-            cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+        #add markers for pupil location to frame image data
+        frame = gaze_tracker.annotated_frame()
 
-            cv2.imshow("Demo", frame)
+        text = "N/A"
+        if gaze_tracker.is_blinking():
+            text = "Blinking"
+        elif gaze_tracker.is_right():
+            text = "Looking right"
+        elif gaze_tracker.is_left():
+            text = "Looking left"
+        elif gaze_tracker.is_center():
+            text = "Looking center"
 
-        if self.ms_elapsed % self.mouse_delay == 0:
-            #update mouse stuff here? with accel/decel
-            var = 0
+        print(text, end=" ")
 
-        self.ms_elapsed += 1
-        
-        #try to perform next update
-        if self.keep_updating:
-            time.sleep(0.001) #pause for 1ms
-            self.update()
+        cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
+
+        left_pupil = gaze_tracker.pupil_left_coords()
+        right_pupil = gaze_tracker.pupil_right_coords()
+        cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+        cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+
+        cv2.imshow("HOTH XII", frame)
+
+    if MS_ELAPSED % MOUSE_DELAY == 0:
+        #update mouse stuff here? with accel/decel
+        _ = 0
+
+    MS_ELAPSED += 1
     
-    def destroy(self):
-        #stop the video recording
-        self.webcam.release()
-        cv2.destroyAllWindows()
-    
-    # def handle_key_press(self, event):
-    #     if event.char == 'q':
-    #         print("Q PRESSED")
-    #         self.keep_updating = False
-    #         self.destroy()
-        
-app = Application() #start the application
+    #try to perform next update
+    print(keep_updating)
+    if keep_updating:
+        time.sleep(0.001) #pause for 1ms
+    else:
+        break
+
+######### DESTRUCTION #############
+#stop the video recording
+webcam.release()
+cv2.destroyAllWindows()
