2daa94a3e1ee8c39031636bd1ca91e6b2c55bf87
Lucas Kalani Ross <lkross@Lucas-MacBook-Pro.local>
2025-03-02 13:29:57 -0800

    distinguishing gaze tracker updates (discrete) from mouse updates (continuous)

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ class Application:
         self.mc = MouseController()
 
         #create a keyboard listener for listening
-        kl = Listener(on_press=self.handle_key_press)
-        kl.start()
+        # kl = Listener(on_press=self.handle_key_press)
+        # kl.start()
 
         #set up gaze tracker
         self.gaze_tracker = GazeTracking()
@@ -21,7 +21,10 @@ class Application:
         
         #delay the gaze tracking by software by x milliseconds so
         #the mouse controller isnt lagging
-        self.ms_delay = 50
+        self.gaze_delay = 50
+        #however we want the mouse movement to be smooth, so it can update instantaneously
+        self.mouse_delay = 10;
+        self.ms_elapsed = 0;
         self.keep_updating = True
 
         # #create a separate thread for processing each frame of
@@ -33,37 +36,43 @@ class Application:
         self.update()
 
     def update(self):
-        print("update")
-        _, frame = self.webcam.read()
+        if self.ms_elapsed % self.gaze_delay == 0:
+            _, frame = self.webcam.read()
 
-        #analyze and update gaze tracker
-        self.gaze_tracker.refresh(frame)
+            #analyze and update gaze tracker
+            self.gaze_tracker.refresh(frame)
 
-        #add markers for pupil location to frame image data
-        frame = self.gaze_tracker.annotated_frame()
+            #add markers for pupil location to frame image data
+            frame = self.gaze_tracker.annotated_frame()
 
-        text = "N/A"
-        if self.gaze_tracker.is_blinking():
-            text = "Blinking"
-        elif self.gaze_tracker.is_right():
-            text = "Looking right"
-        elif self.gaze_tracker.is_left():
-            text = "Looking left"
-        elif self.gaze_tracker.is_center():
-            text = "Looking center"
+            text = "N/A"
+            if self.gaze_tracker.is_blinking():
+                text = "Blinking"
+            elif self.gaze_tracker.is_right():
+                text = "Looking right"
+            elif self.gaze_tracker.is_left():
+                text = "Looking left"
+            elif self.gaze_tracker.is_center():
+                text = "Looking center"
 
-        cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
+            cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
 
-        left_pupil = self.gaze_tracker.pupil_left_coords()
-        right_pupil = self.gaze_tracker.pupil_right_coords()
-        cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
-        cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+            left_pupil = self.gaze_tracker.pupil_left_coords()
+            right_pupil = self.gaze_tracker.pupil_right_coords()
+            cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+            cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
 
-        cv2.imshow("Demo", frame)
+            cv2.imshow("Demo", frame)
 
-        #delay until next update
+        if self.ms_elapsed % self.mouse_delay == 0:
+            #update mouse stuff here? with accel/decel
+            var = 0
+
+        self.ms_elapsed += 1
+        
+        #try to perform next update
         if self.keep_updating:
-            time.sleep(self.ms_delay / 1000)
+            time.sleep(0.001) #pause for 1ms
             self.update()
     
     def destroy(self):
@@ -71,10 +80,10 @@ class Application:
         self.webcam.release()
         cv2.destroyAllWindows()
     
-    def handle_key_press(self, event):
-        if event.char == 'q':
-            print("Q PRESSED")
-            self.keep_updating = False
-            self.destroy()
+    # def handle_key_press(self, event):
+    #     if event.char == 'q':
+    #         print("Q PRESSED")
+    #         self.keep_updating = False
+    #         self.destroy()
         
 app = Application() #start the application
