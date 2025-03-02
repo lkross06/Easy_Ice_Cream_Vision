1fcd9fc4c0cc850b24d4d57f95aedbefa681a2e0
Lucas Kalani Ross <lkross@Lucas-MacBook-Pro.local>
2025-03-02 12:12:07 -0800

    made mouse controller variables private

--- mouse_controller.py
+++ mouse_controller.py
@@ -2,32 +2,32 @@ from pynput.mouse import Button, Controller
 
 class MouseController:
     def __init__(self):
-        self._mouse = Controller()
-        self._isPressing = False
+        self.__mouse = Controller()
+        self.__is_pressing = False
 
     def get_position(self):
         #NOTE: top left corner is (0,0)
-        return self._mouse.position
+        return self.__mouse.position
 
     def set_position(self, x = -1, y = -1):
         #if x or y not specified, don't change it (i.e. set it to its current position)
         if x == -1:
-            x = self._mouse.position[0]
+            x = self.__mouse.position[0]
         elif y == -1:
-            y = self._mouse.position[1]
-        self._mouse.position = (x, y)
+            y = self.__mouse.position[1]
+        self.__mouse.position = (x, y)
     
     def press(self):
         #set guards on whether or not to start pressing or releasing
-        if not self._isPressing:
-            self._mouse.press(Button.left)
-        self._isPressing = True
+        if not self.__is_pressing:
+            self.__mouse.press(Button.left)
+        self.__is_pressing = True
     
     def release(self):
-        if self._isPressing:
-            self._mouse.release(Button.left)
-        self._isPressing = False
+        if self.__is_pressing:
+            self.__mouse.release(Button.left)
+        self.__is_pressing = False
 
     def click(self, n = 1):
         #clicks n times
-        self._mouse.click(Button.left, n)
+        self.__mouse.click(Button.left, n)
