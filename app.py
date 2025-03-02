7fb73259b89d1824662687cc6102cdd21299f461
Lucas Kalani Ross <lkross@Lucas-MacBook-Pro.local>
2025-03-02 12:12:43 -0800

    set up asynchronous gaze tracking in application

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 from tkinter.ttk import *
-import time as t
+import cv2
+import threading
+import time
+
+from gaze_tracking import GazeTracking
 from mouse_controller import MouseController
 
 class Application:
@@ -12,7 +16,7 @@ class Application:
         self.width = self.root.winfo_screenwidth()
         self.height = self.root.winfo_screenheight()
 
-        self.root.title("HOTH_XII")
+        self.root.title("HOTH XII")
         self.root.geometry("{len:.0f}x{len:.0f}+{xoffset:.0f}+{yoffset:.0f}".format(
             len = min(self.width, self.height) * 0.5,
             xoffset = self.width * 0.2,
@@ -20,8 +24,36 @@ class Application:
         ))
         self.root.resizable(width = False, height = False)
 
-        self.root.mainloop()
+        #create a mouse controller for emulation
+        self.mc = MouseController()
 
+        #set up gaze tracker
+        self.gaze_tracker = GazeTracking()
+        self.webcam = cv2.VideoCapture(0) #will feed in frames from the laptop's webcam
+        
+        #delay the gaze tracking by software by x milliseconds so
+        #the mouse controller isnt lagging
+        self.ms_delay = 50
+
+        #create a separate thread for processing each frame of
+        #webcam capture and figuring out pupil position/gaze direction
+        gaze_thread = threading.Thread(target=self.update)
+        gaze_thread.start()
+
+        self.root.mainloop() #tkinter MUST be on main thread
+
+        #stop the video recording
+        self.webcam.release()
+        cv2.destroyAllWindows()
+
+        gaze_thread.join()
+
+    def update(self):
+        print("update")
+
+        time.sleep(self.ms_delay / 1000)
+        self.update()
+        
 app = Application() #start the tkinter application
 
             
